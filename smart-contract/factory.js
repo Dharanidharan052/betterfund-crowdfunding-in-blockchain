103a2178210474bbf6c0296b785066dc484e0c7b
Dharanidharan052 <85599559+Dharanidharan052@users.noreply.github.com>
2022-07-14 16:22:22 +0530

    Add files via upload

--- smart-contract/factory.js
+++ smart-contract/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0x2Bec4B5E67FE9e6Ba5768D83d49a71A60067B813"
+  "0xcaaaeadd0d8e9fc4ac6af8c97faafc11c9f36fc4"
 );
 
 export default instance;
